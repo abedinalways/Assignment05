5b942e6c2d9463adaa4b32a89490a89e9d39c8f4
Sheikh Minhajul Abedin <nion.sk5067@gmail.com>
2025-03-01 12:59:44 +0600

    completed first page

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const today = new Date().toDateString();
 document.getElementById('todayDate').innerHTML = today;
 const completeBtns = document.querySelectorAll('#btn-completed');
+let activities = document.getElementById('activities');
 const taskNumber = document.getElementById('taskNum').innerText;
 taskNum = parseInt(taskNumber);
 const taskDone = document.getElementById('taskDone').innerText;
@@ -8,9 +9,23 @@ numberTask = parseInt(taskDone);
 for (const btn of completeBtns) {
   btn.addEventListener('click', function (event) {
     if (event.target) {
-      event.target.disabled= true;
-      console.dir(event.target);
+      event.target.disabled = true;
+      const activityContainer = event.target.closest('.card-body');
+      const activityTitle = activityContainer.querySelector('h2').innerText;
       
+      const addActivities = document.createElement('div');
+      addActivities.innerHTML = `
+      <div class="bg-green-50 m-2 p-2">
+      <p>"you have completed the task of ${activityTitle} at ${new Date().toTimeString()}"</P>
+      </div>`
+      activities.appendChild(addActivities);
+      const clearHistory = document
+        .getElementById('clear')
+        .addEventListener('click', function (event) {
+          event.preventDefault();
+          activities.removeChild(addActivities);
+        });
+
       taskNum--;
       numberTask++;
       document.getElementById('taskNum').innerText = taskNum;
